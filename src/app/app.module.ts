d00e7794110a97368cdf340e1fb73613b342b062
liamdebellada <liam.debell@ada.ac.uk>
2020-09-05 13:06:43 +0100

    Added HTTP module under modules/login/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { AppCustomLayoutComponent } from './layout/app-custom-layout/app-custom-layout.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, AppCustomLayoutComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/modules/login/login-page/login-page.component.ts
+++ src/app/modules/login/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-page',
@@ -6,19 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-  constructor() { }
 
-  err = ""
+  @ViewChild('loginButton') Loginbutton: ElementRef;
+  @ViewChild('email') emailField: ElementRef;
+  @ViewChild('password') passwordField: ElementRef;
+
+  constructor(private http: HttpClient) { }
+
+  response = {}
+  showStatus = 'Show'
 
   ngOnInit(): void {
   }
 
   changeInputType(input: any): any {
     input.type = input.type === 'password' ? 'text' : 'password';
+    if (input.type === 'password') { this.showStatus = "Show" } else { this.showStatus = "Hide" }
   }
 
+  validateFields() {
+    let email = this.emailField.nativeElement
+    let password = this.passwordField.nativeElement
+
+    if (email.value != "" && password.value != "") {
+      this.Loginbutton.nativeElement.disabled = false
+    } else {
+      this.Loginbutton.nativeElement.disabled = true
+    }
+
+  }
 
   getLoginInfo(loginData) {
-    this.err = loginData['email'] //return errors here using this.err
+    console.log(loginData)
+    this.http.post(
+      'http://localhost:5000/login',
+      loginData,
+      { responseType: 'json' }
+    ).toPromise()
+      .then(
+        res => {
+          this.response = res
+          if (this.response['url'] != undefined) {
+            setTimeout(() => { window.location.href = this.response['url'] }, 2000)
+          }
+        })
+      .catch(error => console.error(error))
   }
+
 }
